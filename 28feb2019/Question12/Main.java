f5410f5d1baf249f2b7968cf5092633583249c96
souvikdgreat <souvikc40@gmail.com>
2019-03-01 14:26:30 +0530

    java 8 assignment

--- 28feb2019/Question12/Main.java
+++ 28feb2019/Question12/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> list= Arrays.asList(4,5,6,7,8,9,0,12,15,16,18,1,2,3);
         System.out.println("Finding the first even number in the integer list which is greater than 3.");
-        Optional<Integer> optionalInteger=list.stream().filter(e->e>3).findFirst();
+        Optional<Integer> optionalInteger=list.stream().filter(e->e>3).filter(e-> e%2==0).findFirst();
         if(optionalInteger.isPresent()){
             System.out.println(optionalInteger.get());
         }
