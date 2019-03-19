83bd460ba7acaf8f59a2105017288d1013f7dc82
souvikdgreat <souvikc40@gmail.com>
2019-03-19 12:34:42 +0530

    springboot assignment added

---
+++ 18mar2019/SpringBootDemo/src/main/java/com/SpringBoot/SpringBootDemo/Entity/Database.java
@@ -0,0 +1,23 @@
+package com.SpringBoot.SpringBootDemo.Entity;
+
+
+public class Database {
+    private int portNo;
+    private String name;
+
+    public int getPortNo() {
+        return portNo;
+    }
+
+    public void setPortNo(int portNo) {
+        this.portNo = portNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
